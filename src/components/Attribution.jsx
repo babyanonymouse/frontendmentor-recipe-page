e23e5e80f1984a96e1005c524038a3f2704b8793
babyanonymouse2@gmail.com <babyanonymouse2@gmail.com>
2025-09-03 16:51:51 +0300

    Add Body and Attribution components for omelette recipe page
    - Created Attribution component for challenge credit.
    - Developed Body component to display the omelette recipe, including preparation time, ingredients, instructions, and nutrition information.
    - Added styling for a clean and responsive layout.
    - Included an image of the omelette for visual appeal.

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Attribution = () => {
+    return (
+        <div className="attribution flex">
+            Challenge by{" "}
+            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer'>
+                Frontend Mentor
+            </a>
+            . Coded by <a href="https://www.yourwebsite.com">Your Name Here</a>.
+        </div>)
+}
+
+export default Attribution
