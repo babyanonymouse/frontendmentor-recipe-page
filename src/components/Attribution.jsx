efb62a265a33ac3958b045570c62dc01b53f730f
babyanonymouse2@gmail.com <babyanonymouse2@gmail.com>
2025-09-03 17:05:31 +0300

    Refactor Attribution component for improved styling and correct links

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Attribution = () => {
     return (
-        <div className="attribution flex">
+        <div className="my-2 attribution text-sm text-center">
             Challenge by{" "}
-            <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel='noopener noreferrer'>
-                Frontend Mentor
+            <a href="https://www.frontendmentor.io/challenges/recipe-page-KiTsR8QQKm" target="_blank" rel='noopener noreferrer' className='text-[#3E52A3]'>Frontend Mentor
             </a>
-            . Coded by <a href="https://www.yourwebsite.com">Your Name Here</a>.
-        </div>)
+            . Coded by <a href="https://babyanonymouse.github.io/Portfolio/" className='text-[#3E52A3]'>Samuel Lwanga</a>.
+        </div>
+    )
 }
 
 export default Attribution
