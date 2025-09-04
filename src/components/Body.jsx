2f29edee3997232dd327ade0488da89750774382
babyanonymouse2@gmail.com <babyanonymouse2@gmail.com>
2025-09-04 09:02:37 +0300

    Enhance styling of instructions list in Body component for better visibility

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -34,7 +34,7 @@ const Body = () => {
 
                 <div>
                     <h2 className='text-brown-800 font-young-serif text-3xl my-3'>Instructions</h2>
-                    <ol className='list-decimal pl-5 marker:text-brown-800 space-y-2'>
+                    <ol className='list-decimal pl-5 marker:text-brown-800 marker:font-bold space-y-2'>
                         <li><strong>Beat the eggs:</strong> In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture.</li>
                         <li><strong>Heat the pan:</strong> Place a non-stick frying pan over medium heat and add butter or oil.</li>
                         <li><strong>Cook the omelette:</strong> Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface.</li>
